781ecadd75f7c9a23346fc124a7593e304ca5944
Gwenael Ansermoz <gw.ansermoz@pm.me>
2020-02-26 11:29:24 +0100

    added currency loading

--- src/component/menu/menu.jsx
+++ src/component/menu/menu.jsx
@@ -3,14 +3,33 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
+import Axios from "axios";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 
 export class Menu extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      selectedCurrency: 0,
+      currencies: [],
+    }
+  }
+
+  componentDidMount() {
+    this.loadData()
+  }
+
+  loadData() {
+    Axios.get("/data.json").then(res => {
+      this.setState({
+        currencies: Object.keys(res.data)
+      })
+    })
   }
 
   render() {
+    let currencies = this.renderSelectCurrencies();
     return (
       <AppBar position={"static"} color={"transparent"}>
         <Toolbar>
@@ -24,7 +43,9 @@ export class Menu extends React.Component {
             </Grid>
 
             <Grid item>
-              <Button>CHF</Button>
+              <Select value={this.state.selectedCurrency} onChange={this.handleCurrencyChange}>
+                {currencies}
+              </Select>
             </Grid>
 
             <Grid item>
@@ -36,5 +57,17 @@ export class Menu extends React.Component {
 
       </AppBar>
     )
+  }
+
+  renderSelectCurrencies() {
+    return this.state.currencies.map((value, idx) =>
+      <MenuItem key={idx} value={idx}>{value}</MenuItem>
+    )
+  }
+
+  handleCurrencyChange = event => {
+    this.setState({
+      selectedCurrency: event.target.value
+    })
   }
 }
