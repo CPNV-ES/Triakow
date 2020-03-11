5f7ce8bf193b0f183d545996098b19525987166e
Gwenael.ANSERMOZ <gw.ansermoz@pm.me>
2020-03-11 08:29:48 +0100

    turned Headline into a function component

--- src/component/headline/headline.jsx
+++ src/component/headline/headline.jsx
@@ -4,26 +4,18 @@ import "./headline.css"
 import Logo from "./Trivago.png";
 import Typography from "@material-ui/core/Typography";
 
-
-export class Headline extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {}
-  }
-
-  render() {
-    return (
-      <Grid id="headline" container spacing={3} justify={"center"}>
-        <Grid item>
-          <img alt={"Trivago logo"} id="logo" src={Logo}/>
-        </Grid>
-
-        <Grid item>
-          <Typography variant="h5"><strong>Trouvez l’hôtel idéal et comparez les prix de différents sites
-            Web</strong></Typography>
-          <Typography variant="p">Essayez de rechercher une ville, un hôtel ou même un lieu connu.</Typography>
-        </Grid>
+export function Headline() {
+  return (
+    <Grid id="headline" container spacing={3} justify={"center"}>
+      <Grid item>
+        <img alt={"Trivago logo"} id="logo" src={Logo}/>
       </Grid>
-    )
-  }
+
+      <Grid item>
+        <Typography variant="h5"><strong>Trouvez l’hôtel idéal et comparez les prix de différents sites
+          Web</strong></Typography>
+        <Typography variant="p">Essayez de rechercher une ville, un hôtel ou même un lieu connu.</Typography>
+      </Grid>
+    </Grid>
+  )
 }
