afaaaa0296f1304b8a07067db170f6fef9179a05
Gwenael.ANSERMOZ <gw.ansermoz@pm.me>
2020-03-11 08:36:02 +0100

    turned Search into a function component

--- src/component/search/search.jsx
+++ src/component/search/search.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Select from "@material-ui/core/Select";
@@ -7,71 +7,41 @@ import {DatePicker} from "@material-ui/pickers";
 import moment from 'moment'
 import Grid from "@material-ui/core/Grid";
 
-export class Search extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      city: "",
-      room: 0,
-      startAt: moment(),
-      endAt: moment(),
-    }
-  }
 
-  render() {
-    return (
-      <Grid container spacing={3} justify={"center"}>
-        <Grid item>
-          <TextField value={this.state.city} onChange={this.handleCityChange} placeholder="Londres"/>
-        </Grid>
+export function Search() {
+  const [city, setCity] = useState("");
+  const [room, setRoom] = useState(0);
+  const [startAt, setStartAt] = useState(moment());
+  const [endAt, setEndAt] = useState(moment());
 
-        <Grid item>
-          <DatePicker value={this.state.startAt} onChange={this.handleStartAtChange}/>
-        </Grid>
-
-        <Grid item>
-          <DatePicker value={this.state.endAt} onChange={this.handleEndAtChange}/>
-        </Grid>
-
-        <Grid item>
-          <Select value={this.state.room} onChange={this.handleRoomChange}>
-            <MenuItem value={0}>Chambre simple</MenuItem>
-            <MenuItem value={1}>Chambre double</MenuItem>
-            <MenuItem value={2}>Chambres familiales</MenuItem>
-            <MenuItem value={3}>Chambres multiples</MenuItem>
-          </Select>
-        </Grid>
-
-        <Grid item>
-          <Button variant="contained" color="primary">
-            Chercher
-          </Button>
-        </Grid>
+  return (
+    <Grid container spacing={3} justify={"center"}>
+      <Grid item>
+        <TextField value={city} onChange={(event => setCity(event.target.value))} placeholder="Londres"/>
       </Grid>
-    )
-  }
 
-  handleCityChange = event => {
-    this.setState({
-      city: event.target.value
-    })
-  };
+      <Grid item>
+        <DatePicker value={startAt} onChange={setStartAt}/>
+      </Grid>
 
-  handleRoomChange = event => {
-    this.setState({
-      room: event.target.value
-    })
-  };
+      <Grid item>
+        <DatePicker value={endAt} onChange={setEndAt}/>
+      </Grid>
 
-  handleStartAtChange = date => {
-    this.setState({
-      startAt: date
-    })
-  };
+      <Grid item>
+        <Select value={room} onChange={event => setRoom(event.target.value)}>
+          <MenuItem value={0}>Chambre simple</MenuItem>
+          <MenuItem value={1}>Chambre double</MenuItem>
+          <MenuItem value={2}>Chambres familiales</MenuItem>
+          <MenuItem value={3}>Chambres multiples</MenuItem>
+        </Select>
+      </Grid>
 
-  handleEndAtChange = date => {
-    this.setState({
-      endAt: date
-    })
-  }
+      <Grid item>
+        <Button variant="contained" color="primary">
+          Chercher
+        </Button>
+      </Grid>
+    </Grid>
+  );
 }
