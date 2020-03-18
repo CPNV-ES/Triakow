6726dd116804c9e2a6f0abff07306ee809935308
dlmw <12473240+dlmw@users.noreply.github.com>
2020-03-18 10:20:11 +0100

    Use of ternary operator for ease of reading

--- src/component/headline/headline.jsx
+++ src/component/headline/headline.jsx
@@ -10,7 +10,6 @@ export function Headline() {
   const {t} = useTranslation();
 
   const displayLogo = useMediaQuery('(min-width:980px)');
-  const bigScreen = useMediaQuery('(min-width:600px)')
   const smallScreen = useMediaQuery('(max-width:600px)');
 
   return (
@@ -20,7 +19,7 @@ export function Headline() {
       </Grid>
 
       <Grid item>
-        <Typography variant="h5"><strong>{smallScreen && t("headline.pitch.small")} {bigScreen && t("headline.pitch.big")}</strong></Typography>
+        <Typography variant="h5"><strong>{smallScreen ? t("headline.pitch.small") : t("headline.pitch.big")}</strong></Typography>
         <Typography variant="body1">{t("headline.try")}</Typography>
       </Grid>
     </Grid>
