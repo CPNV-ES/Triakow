90947acbf82172b63070f8e7f2222dee966f74a5
dlmw <12473240+dlmw@users.noreply.github.com>
2020-04-01 08:45:32 +0200

    Display icon instead of text for burger menu

--- src/component/burger/burger.jsx
+++ src/component/burger/burger.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { bool, func } from 'prop-types';
 import { StyledBurger } from './burger.styled';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const Burger = ({ open, setOpen }) => {
     return (
         <StyledBurger open={open} onClick={() => setOpen(!open)}>
-            <div />Burger Menu
-            <div />
-            <div />
+            <MenuIcon/>
         </StyledBurger>
     )
 }
